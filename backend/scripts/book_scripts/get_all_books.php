8615fe37b066f9dd3ed7b35fa9a10856d31f4a2e
merima <merima.maksumic@stu.ibu.edu.ba>
2024-04-22 20:58:42 +0200

    baza, dashboard, books

---
+++ backend/scripts/book_scripts/get_all_books.php
@@ -0,0 +1,11 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+require_once __DIR__ . '/../../rest/services/BookService.class.php';
+
+$book_service = new BookService();
+$books = $book_service->get_all_books();
+
+// Wrap the result in a 'books' key
+$response = ["books" => $books];
+echo json_encode($response);
